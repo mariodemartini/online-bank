023bffa705c9db4190d2c0abb779ed8c7d54b54a
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-03-27 16:14:25 -0300

    :tada: initial commit

---
+++ src/test/java/br/com/geradordedevs/picpaychallenge/PicPayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.picpaychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
