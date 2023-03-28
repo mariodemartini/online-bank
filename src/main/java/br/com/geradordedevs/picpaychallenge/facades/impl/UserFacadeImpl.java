c67cdb810a31f6b64646479205c115f323ebe9e9
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-03-28 10:34:04 -0300

    :sparkles: feat: implementing transaction method

--- src/main/java/br/com/geradordedevs/picpaychallenge/facades/impl/UserFacadeImpl.java
+++ src/main/java/br/com/geradordedevs/picpaychallenge/facades/impl/UserFacadeImpl.java
@@ -1,13 +1,17 @@
 package br.com.geradordedevs.picpaychallenge.facades.impl;
 
+import br.com.geradordedevs.picpaychallenge.dtos.requests.TransactionRequestDTO;
 import br.com.geradordedevs.picpaychallenge.dtos.requests.UserRequestDTO;
 import br.com.geradordedevs.picpaychallenge.dtos.responses.UserResponseDTO;
+import br.com.geradordedevs.picpaychallenge.entities.UserEntity;
+import br.com.geradordedevs.picpaychallenge.enums.DocumentTypeEnum;
 import br.com.geradordedevs.picpaychallenge.facades.UserFacade;
 import br.com.geradordedevs.picpaychallenge.mappers.UserMapper;
 import br.com.geradordedevs.picpaychallenge.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Component
@@ -42,5 +46,26 @@ public class UserFacadeImpl implements UserFacade {
     @Override
     public void deleteUser(Long id) {
         userService.deleteUser(id);
+    }
+
+    @Override
+    public void transaction(TransactionRequestDTO transactionRequestDTO) throws Exception {
+        Long idPayer = transactionRequestDTO.getPayer();
+        Long idPayee = transactionRequestDTO.getPayee();
+        BigDecimal value = transactionRequestDTO.getValue();
+
+        UserEntity userPayer = userService.findById(idPayer);
+        if(userPayer.getDocumentTypeEnum() == DocumentTypeEnum.CNPF){
+            throw new Exception("invalid transaction");
+        }
+        UserEntity userPayee = userService.findById(idPayee);
+
+        userPayer.setId(idPayer);
+        userPayer.setBalance(userPayer.getBalance().subtract(value));
+        userService.updateUser(idPayer, userPayer);
+
+        userPayee.setId(idPayee);
+        userPayee.setBalance(userPayee.getBalance().add(value));
+        userService.updateUser(idPayee, userPayee);
     }
 }
