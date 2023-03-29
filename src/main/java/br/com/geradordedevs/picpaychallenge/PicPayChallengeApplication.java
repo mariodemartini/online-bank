5d35a27f5d489412359a65840e3f677e1e1a1a0a
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-03-29 13:22:07 -0300

    :sparkles: feat: adding anotation of OpenFeign

--- src/main/java/br/com/geradordedevs/picpaychallenge/PicPayChallengeApplication.java
+++ src/main/java/br/com/geradordedevs/picpaychallenge/PicPayChallengeApplication.java
@@ -2,10 +2,13 @@ package br.com.geradordedevs.picpaychallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableFeignClients
 public class PicPayChallengeApplication {
 
 	public static void main(String[] args) {
