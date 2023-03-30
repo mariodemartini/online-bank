b32fb86324d46884bf9e1804d8d4f37ec1777edf
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-03-30 15:21:13 -0300

    :sparkles: feat: adding @Bean PasswordEncoder

--- src/main/java/br/com/geradordedevs/picpaychallenge/PicPayChallengeApplication.java
+++ src/main/java/br/com/geradordedevs/picpaychallenge/PicPayChallengeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
@@ -19,5 +20,11 @@ public class PicPayChallengeApplication {
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 }
