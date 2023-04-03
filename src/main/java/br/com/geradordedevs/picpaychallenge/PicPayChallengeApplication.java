20f592348be39b1a353caa0e31060baad72851e0
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-04-03 14:42:40 -0300

    :recycle: refactor: adding anotation @OpenAPIDefinition

--- src/main/java/br/com/geradordedevs/picpaychallenge/PicPayChallengeApplication.java
+++ src/main/java/br/com/geradordedevs/picpaychallenge/PicPayChallengeApplication.java
@@ -1,15 +1,18 @@
 package br.com.geradordedevs.picpaychallenge;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "PicPay Challenge API"))
 public class PicPayChallengeApplication {
 
 	public static void main(String[] args) {
